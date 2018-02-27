d9bb6baeecec134c15695c1d2af255246bd2494b
TimLoveFreedom <zhangguodong_12@126.com>
2018-02-27 17:15:26 +0800

    init

---
+++ mlp/mlp.py
@@ -0,0 +1,7 @@
+__author__ = 'jellyzhang'
+class Mlp(object):
+    def __init__(self,layer_numer=3,hidden_units=[200,100,300]):
+        self.layer_numer-layer_numer #层数
+        self.hidden_units=hidden_units #每层对应的单元数
+
+
