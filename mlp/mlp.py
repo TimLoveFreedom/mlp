af3381a30c87eba7d6e2566c467fe553f3e69a4c
TimLoveFreedom <zhangguodong_12@126.com>
2018-02-27 17:23:29 +0800

    init

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -1,4 +1,9 @@
 __author__ = 'jellyzhang'
+import tensorflow as tf
+import numpy as np
+'''
+tensorflow实现的mlp类
+'''
 class Mlp(object):
     def __init__(self,layer_numer=3,hidden_units=[200,100,300]):
         self.layer_numer-layer_numer #层数
---
+++ randomforest/randomforest.py
@@ -0,0 +1,9 @@
+__author__ = 'jellyzhang'
+from sklearn.ensemble import RandomForestClassifier
+'''
+随机森林类
+'''
+
+class RandomClassifier(object):
+    def __init__(self):
+        pass
